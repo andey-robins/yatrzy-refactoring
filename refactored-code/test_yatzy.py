258bfed53a7fd9748173163cd11c7d64c71839ce
andey <jtuttle5@uwyo.edu>
2020-08-13 14:26:07 -0600

    Finish all scoring refactoring

--- refactored-code/test_yatzy.py
+++ refactored-code/test_yatzy.py
@@ -5,17 +5,17 @@ from yatzy import Hand, Scoring, Yatzy
 
 
 def test_chance():
-    assert 5 == Yatzy.chance(1, 1, 1, 1, 1)
-    assert 15 == Yatzy.chance(2, 3, 4, 5, 1)
-    assert 16 == Yatzy.chance(3, 3, 4, 5, 1)
-    assert 30 == Yatzy.chance(6, 6, 6, 6, 6)
+    assert 5 == Scoring.chance(Hand(1, 1, 1, 1, 1))
+    assert 15 == Scoring.chance(Hand(2, 3, 4, 5, 1))
+    assert 16 == Scoring.chance(Hand(3, 3, 4, 5, 1))
+    assert 30 == Scoring.chance(Hand(6, 6, 6, 6, 6))
 
 
 def test_yatzy():
-    assert 50 == Yatzy.yatzy([4, 4, 4, 4, 4])
-    assert 50 == Yatzy.yatzy([6, 6, 6, 6, 6])
-    assert 0 == Yatzy.yatzy([6, 6, 6, 6, 3])
-    assert 0 == Yatzy.yatzy([1, 2, 3, 4, 5])
+    assert 50 == Scoring.yatzy(Hand(4, 4, 4, 4, 4))
+    assert 50 == Scoring.yatzy(Hand(6, 6, 6, 6, 6))
+    assert 0 == Scoring.yatzy(Hand(6, 6, 6, 6, 3))
+    assert 0 == Scoring.yatzy(Hand(1, 2, 3, 4, 5))
 
 
 def test_ones():
@@ -58,44 +58,45 @@ def test_sixes():
 
 
 def test_one_pair():
-    assert 6 == Yatzy.score_pair(3, 4, 3, 5, 6)
-    assert 10 == Yatzy.score_pair(5, 3, 3, 3, 5)
-    assert 12 == Yatzy.score_pair(5, 3, 6, 6, 5)
-    assert 0 == Yatzy.score_pair(1, 2, 3, 4, 5)
+    assert 6 == Scoring.pair(Hand(3, 4, 3, 5, 6))
+    assert 10 == Scoring.pair(Hand(5, 3, 3, 3, 5))
+    assert 12 == Scoring.pair(Hand(5, 3, 6, 6, 5))
+    assert 0 == Scoring.pair(Hand(1, 2, 3, 4, 5))
 
 
 def test_two_pair():
-    assert 16 == Yatzy.two_pair(3, 3, 5, 4, 5)
-    assert 18 == Yatzy.two_pair(3, 3, 6, 6, 6)
-    assert 0 == Yatzy.two_pair(3, 3, 6, 5, 4)
+    assert 16 == Scoring.two_pair(Hand(3, 3, 5, 4, 5))
+    assert 18 == Scoring.two_pair(Hand(3, 3, 6, 6, 6))
+    assert 0 == Scoring.two_pair(Hand(3, 3, 6, 5, 4))
 
 
 def test_three_of_a_kind():
-    assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 4, 5)
-    assert 15 == Yatzy.three_of_a_kind(5, 3, 5, 4, 5)
-    assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 3, 5)
+    assert 9 == Scoring.three_of_a_kind(Hand(3, 3, 3, 4, 5))
+    assert 15 == Scoring.three_of_a_kind(Hand(5, 3, 5, 4, 5))
+    assert 9 == Scoring.three_of_a_kind(Hand(3, 3, 3, 3, 5))
+    assert 0 == Scoring.three_of_a_kind(Hand(3, 3, 2, 2, 1))
 
 
 def test_four_of_a_kind():
-    assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 5)
-    assert 20 == Yatzy.four_of_a_kind(5, 5, 5, 4, 5)
-    assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 3)
-    assert 0 == Yatzy.four_of_a_kind(3, 3, 3, 2, 1)
+    assert 12 == Scoring.four_of_a_kind(Hand(3, 3, 3, 3, 5))
+    assert 20 == Scoring.four_of_a_kind(Hand(5, 5, 5, 4, 5))
+    assert 12 == Scoring.four_of_a_kind(Hand(3, 3, 3, 3, 3))
+    assert 0 == Scoring.four_of_a_kind(Hand(3, 3, 3, 2, 1))
 
 
 def test_small_straight():
-    assert 15 == Yatzy.smallStraight(1, 2, 3, 4, 5)
-    assert 15 == Yatzy.smallStraight(2, 3, 4, 5, 1)
-    assert 0 == Yatzy.smallStraight(1, 2, 2, 4, 5)
+    assert 15 == Scoring.small_straight(Hand(1, 2, 3, 4, 5))
+    assert 15 == Scoring.small_straight(Hand(2, 3, 4, 5, 1))
+    assert 0 == Scoring.small_straight(Hand(1, 2, 2, 4, 5))
 
 
 def test_large_straight():
-    assert 20 == Yatzy.largeStraight(6, 2, 3, 4, 5)
-    assert 20 == Yatzy.largeStraight(2, 3, 4, 5, 6)
-    assert 0 == Yatzy.largeStraight(1, 2, 2, 4, 5)
+    assert 20 == Scoring.large_straight(Hand(6, 2, 3, 4, 5))
+    assert 20 == Scoring.large_straight(Hand(2, 3, 4, 5, 6))
+    assert 0 == Scoring.large_straight(Hand(1, 2, 2, 4, 5))
 
 
 def test_full_house():
-    assert 18 == Yatzy.fullHouse(6, 2, 2, 2, 6)
-    assert 0 == Yatzy.fullHouse(2, 3, 4, 5, 6)
+    assert 18 == Scoring.full_house(Hand(6, 2, 2, 2, 6))
+    assert 0 == Scoring.full_house(Hand(2, 3, 4, 5, 6))
 
--- refactored-code/yatzy.py
+++ refactored-code/yatzy.py
@@ -33,6 +33,96 @@ class Scoring:
     def sixes(hand):
         return single_number(hand, 6)
 
+    @staticmethod
+    def chance(hand):
+        return sum(hand.dice)
+
+    @staticmethod
+    def yatzy(hand):
+        firstDie = hand.dice[0]
+        for i in range(1, len(hand.dice)):
+            if hand.dice[i] != firstDie:
+                return 0
+        return 50
+
+    @staticmethod
+    def pair(hand):
+        counts = dict.fromkeys(range(1, 7), 0)
+        for die in hand.dice:
+            counts[die] += 1
+
+        for location in reversed(range(1, 7)):
+            if counts[location] >= 2:
+                return location * 2
+        return 0
+
+    @staticmethod
+    def two_pair(hand):
+        counts = dict.fromkeys(range(1, 7), 0)
+        for die in hand.dice:
+            counts[die] += 1
+
+        pairs = 0
+        score = 0
+
+        for location in reversed(range(1, 7)):
+            if counts[location] >= 2:
+                pairs += 1
+                score += location * 2
+
+        if pairs == 2:
+            return score
+        else:
+            return 0
+
+    @staticmethod
+    def three_of_a_kind(hand):
+        counts = dict.fromkeys(range(1, 7), 0)
+        for die in hand.dice:
+            counts[die] += 1
+
+        for location in reversed(range(1, 7)):
+            if counts[location] >= 3:
+                return location * 3
+        return 0
+
+    @staticmethod
+    def four_of_a_kind(hand):
+        counts = dict.fromkeys(range(1, 7), 0)
+        for die in hand.dice:
+            counts[die] += 1
+
+        for location in reversed(range(1, 7)):
+            if counts[location] >= 4:
+                return location * 4
+        return 0
+
+    @staticmethod
+    def small_straight(hand):
+        hand.dice.sort()
+        if hand.dice == [1, 2, 3, 4, 5]:
+            return 15
+        else:
+            return 0
+
+    @staticmethod
+    def large_straight(hand):
+        hand.dice.sort()
+        if hand.dice == [2, 3, 4, 5, 6]:
+            return 20
+        else:
+            return 0
+
+    @staticmethod
+    def full_house(hand):
+        counts = dict.fromkeys(range(1, 7), 0)
+        for die in hand.dice:
+            counts[die] += 1
+
+        if 2 in counts.values() and 3 in counts.values():
+            return sum(hand.dice)
+        return 0
+
 
 def single_number(hand, number):
     sum = 0
@@ -40,152 +130,3 @@ def single_number(hand, number):
         if die == number:
             sum += die
     return sum
-
-
-class Yatzy:
-    @staticmethod
-    def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
-
-    @staticmethod
-    def yatzy(dice):
-        counts = [0] * (len(dice) + 1)
-        for die in dice:
-            counts[die - 1] += 1
-        for i in range(len(counts)):
-            if counts[i] == 5:
-                return 50
-        return 0
-
-    @staticmethod
-    def score_pair(d1, d2, d3, d4, d5):
-        counts = [0] * 6
-        counts[d1 - 1] += 1
-        counts[d2 - 1] += 1
-        counts[d3 - 1] += 1
-        counts[d4 - 1] += 1
-        counts[d5 - 1] += 1
-        at = 0
-        for at in range(6):
-            if counts[6 - at - 1] == 2:
-                return (6 - at) * 2
-        return 0
-
-    @staticmethod
-    def two_pair(d1, d2, d3, d4, d5):
-        counts = [0] * 6
-        counts[d1 - 1] += 1
-        counts[d2 - 1] += 1
-        counts[d3 - 1] += 1
-        counts[d4 - 1] += 1
-        counts[d5 - 1] += 1
-        n = 0
-        score = 0
-        for i in range(6):
-            if counts[6 - i - 1] >= 2:
-                n = n + 1
-                score += 6 - i
-
-        if n == 2:
-            return score * 2
-        else:
-            return 0
-
-    @staticmethod
-    def four_of_a_kind(_1, _2, d3, d4, d5):
-        tallies = [0] * 6
-        tallies[_1 - 1] += 1
-        tallies[_2 - 1] += 1
-        tallies[d3 - 1] += 1
-        tallies[d4 - 1] += 1
-        tallies[d5 - 1] += 1
-        for i in range(6):
-            if tallies[i] >= 4:
-                return (i + 1) * 4
-        return 0
-
-    @staticmethod
-    def three_of_a_kind(d1, d2, d3, d4, d5):
-        t = [0] * 6
-        t[d1 - 1] += 1
-        t[d2 - 1] += 1
-        t[d3 - 1] += 1
-        t[d4 - 1] += 1
-        t[d5 - 1] += 1
-        for i in range(6):
-            if t[i] >= 3:
-                return (i + 1) * 3
-        return 0
-
-    @staticmethod
-    def smallStraight(d1, d2, d3, d4, d5):
-        tallies = [0] * 6
-        tallies[d1 - 1] += 1
-        tallies[d2 - 1] += 1
-        tallies[d3 - 1] += 1
-        tallies[d4 - 1] += 1
-        tallies[d5 - 1] += 1
-        if (
-            tallies[0] == 1
-            and tallies[1] == 1
-            and tallies[2] == 1
-            and tallies[3] == 1
-            and tallies[4] == 1
-        ):
-            return 15
-        return 0
-
-    @staticmethod
-    def largeStraight(d1, d2, d3, d4, d5):
-        tallies = [0] * 6
-        tallies[d1 - 1] += 1
-        tallies[d2 - 1] += 1
-        tallies[d3 - 1] += 1
-        tallies[d4 - 1] += 1
-        tallies[d5 - 1] += 1
-        if (
-            tallies[1] == 1
-            and tallies[2] == 1
-            and tallies[3] == 1
-            and tallies[4] == 1
-            and tallies[5] == 1
-        ):
-            return 20
-        return 0
-
-    @staticmethod
-    def fullHouse(d1, d2, d3, d4, d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0] * 6
-        tallies[d1 - 1] += 1
-        tallies[d2 - 1] += 1
-        tallies[d3 - 1] += 1
-        tallies[d4 - 1] += 1
-        tallies[d5 - 1] += 1
-
-        for i in range(6):
-            if tallies[i] == 2:
-                _2 = True
-                _2_at = i + 1
-
-        for i in range(6):
-            if tallies[i] == 3:
-                _3 = True
-                _3_at = i + 1
-
-        if _2 and _3:
-            return _2_at * 2 + _3_at * 3
-        else:
-            return 0
